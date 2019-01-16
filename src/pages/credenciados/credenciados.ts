f14e21f7ef51145946f83a036f0ff61de5965849
Mateus Pereira <mateus.silva@datainfo.inf.br>
2019-01-16 11:06:38 -0200

    integração credenciados com sistema de gestão andes

--- src/pages/credenciados/credenciados.ts
+++ src/pages/credenciados/credenciados.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { GrupoPage } from '../grupo/grupo';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
 
 /**
  * Generated class for the CredenciadosPage page.
@@ -16,37 +18,40 @@ import { GrupoPage } from '../grupo/grupo';
 }
 )
 export class CredenciadosPage {
-  credenciado: Array<Object> = [];
+
+  public teste = "Buscar...";
   
   
+  url = "http://webtecsites.com.br/api/doc/credeciamento";
+  
+  obs: Observable<any>;
 
   searchQuery: string = '';
-  // items: string[];
+
   items = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-   
-    // for (let index = 0; index < 10; index++) {
-    //   this.aaa = Array.length;
-    //   this.credenciado[index] = {
-    //     results:{
-    //      titulo: "fulano"+index,
-    //      descricao: "300"+index,
-    //   }
- 
-    //   };
+  grupo = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public HttpClient: HttpClient) {
+
+      this.obs = this.HttpClient.get(this.url);
+
+      this.obs.
+          subscribe(data =>{
+              this.items = data['results'];
+              this.grupo = this.items;
+          })
       
-    //  }
-    //  console.log(this.credenciado+this.aaa);
-
-    
-
   }
 
   doRefresh(refresher) {
     console.log('Begin async operation', refresher);
 
     setTimeout(() => {
-      console.log('Async operation has ended');
+      this.obs.
+          subscribe(data =>{
+              this.items = data['results'];
+              this.grupo = this.items;
+          })
       refresher.complete();
     }, 2000);
   }
@@ -56,88 +61,9 @@ export class CredenciadosPage {
 
   initializeItems() {
     this.items = [];
-    this.credenciado = [];
-    // for (let index = 0; index < 10; index++) {
-      
-    //   this.credenciado.push(
-    //     ["Grupo"+index, ["300"]]
-    //   );
-       
-    //    this.items.push(["Fulano"+index, ["300"+index],["Grupo"+index],["Seção Sindical: cidade"+index]]);
-       
-       
-    //  }
-     
-    this.items = [
-      
-        
-        {
-        "id": 1,
-        "name": "Danismar Souza da Paz",
-        "grupo": {
-        "id": 1,
-        "name": "Grupo 10",
-        "sala": "1245",
-        "created_at": "2014-01-01T00:00:00+0000",
-        "updated_at": "2014-01-01T00:00:00+0000"
-        },
-        "sessao_sindical": "Brasilia/DF",
-        "slug": "danismar-souza-da-paz",
-        "created_at": "2019-01-12T20:09:20+0000",
-        "updated_at": "2019-01-12T20:14:03+0000"
-        },
-        {
-        "id": 2,
-        "name": "Mateus Pereira da Silva",
-        "grupo": {
-        "id": 2,
-        "name": "Grupo 05",
-        "sala": "1222",
-        "created_at": "2014-01-01T00:00:00+0000",
-        "updated_at": "2014-01-01T00:00:00+0000"
-        },
-        "sessao_sindical": "Goiania/GO",
-        "slug": "mateus-pereira-da-silva",
-        "created_at": "2019-01-12T20:26:31+0000",
-        "updated_at": "2019-01-12T20:26:31+0000"
-        },
-        {
-        "id": 3,
-        "name": "Carlos Eduardo Batista",
-        "grupo": {
-        "id": 2,
-        "name": "Grupo 05",
-        "sala": "1222",
-        "created_at": "2014-01-01T00:00:00+0000",
-        "updated_at": "2014-01-01T00:00:00+0000"
-        },
-        "sessao_sindical": "Belo Horizonte/MG",
-        "slug": "carlos-eduardo-batista",
-        "created_at": "2019-01-13T14:53:13+0000",
-        "updated_at": "2019-01-13T14:53:13+0000"
-        },
-        {
-        "id": 4,
-        "name": "Luciana da Luz",
-        "grupo": {
-        "id": 1,
-        "name": "Grupo 10",
-        "sala": "1245",
-        "created_at": "2014-01-01T00:00:00+0000",
-        "updated_at": "2014-01-01T00:00:00+0000"
-        },
-        "sessao_sindical": "Palmas/TO",
-        "slug": "luciana-da-luz",
-        "created_at": "2019-01-14T18:22:55+0000",
-        "updated_at": "2019-01-14T18:32:02+0000"
-        }
-        ];
-     
-     
-     //console.log(this.items[1]);
-    //  console.log(this.items);
+    this.items = this.grupo;
   }
-
+  
   getItems(ev: any) {
     // Reset items back to all of the items
     this.initializeItems();
@@ -150,7 +76,7 @@ export class CredenciadosPage {
       if (val && val.trim() != '') {
         this.items = this.items.filter((item) => {
           
-          return (item['name'].toLowerCase().indexOf(val.toLowerCase()) > -1);
+           return (item['name'].toLowerCase().indexOf(val.toLowerCase()) > -1);
            
         })
       }
@@ -167,5 +93,7 @@ export class CredenciadosPage {
         
     });
   }
+
+  
 
 }
